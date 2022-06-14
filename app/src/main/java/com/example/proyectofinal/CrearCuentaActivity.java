c48e234d12a9820336bd7f13850e94401992606e
ecg31 <eduardocoriaguerrero31099@gmail.com>
2022-06-14 13:09:12 -0500

    se añadio diseño de la app

--- app/src/main/java/com/example/proyectofinal/CrearCuentaActivity.java
+++ app/src/main/java/com/example/proyectofinal/CrearCuentaActivity.java
@@ -1,14 +1,36 @@
 package com.example.proyectofinal;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.proyectofinal.databinding.ActivityCrearCuentaBinding;
 
 public class CrearCuentaActivity extends AppCompatActivity {
+
+    private ActivityCrearCuentaBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crear_cuenta);
+        binding = ActivityCrearCuentaBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        setListeners();
+
+    }
+
+    //Método el botón login
+    public void login(View view) {
+        Intent login = new Intent(this, MainActivity.class);
+        startActivity(login);
+        finish();
+    }
+
+    private void setListeners() {
+        binding.toolbar.setNavigationOnClickListener(this::login );
     }
 }
--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.proyectofinal;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.proyectofinal.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        
+        setListeners();
+
+    }
+
+    //Método el botón crearCuenta
+    public void crearCuenta(View view){
+        Intent crearCuenta = new Intent(this, CrearCuentaActivity.class);
+        startActivity(crearCuenta);
+        finish();
+    }
+    //Método el botón login
+    public void login(View view){
+        Intent login = new Intent(this, DrawerActivity.class);
+        startActivity(login);
+        finish();
+    }
+
+    private void setListeners() {
+        binding.btnCreateAccount.setOnClickListener(this::crearCuenta);
+        binding.btnLogin.setOnClickListener(this::login);
+
     }
 }

c6566e03895737a373eaf9ca41dadcd9b1081b9e
OremGar <garca_orlando@yahoo.com>
2022-06-19 13:42:01 -0500

    Lectura QR y Mapas

---
+++ app/src/main/java/com/example/proyectofinal/CodigoQR.java
@@ -0,0 +1,54 @@
+package com.example.proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.integration.android.IntentIntegrator;
+
+public class CodigoQR extends AppCompatActivity
+{
+    //Texto que muestra el resultado
+    //Depuración
+    private TextView txtResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_codigo_qr);
+
+        //Referencia de elementos de diseño
+        Button btnEscanear = (Button) findViewById(R.id.btnEscanear);
+        txtResultado = (TextView) findViewById(R.id.txtResultado);
+
+        btnEscanear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                IntentIntegrator intento = new IntentIntegrator(CodigoQR.this);       //Intento para la integración del lector QR al activity
+                intento.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);           //Se aceptan todos los tipos de códigos
+                intento.setPrompt("Scan");                                                   //Mensaje
+                intento.setCameraId(0);                                                      //Se selecciona la camara
+                intento.setBeepEnabled(true);                                                //Sonido al registrar código
+                intento.setBarcodeImageEnabled(false);                                       //Habilitar el guardado de la imagén
+                intento.initiateScan();                                                      //Se inicia el scanner
+
+            }
+        });
+    }
+
+    //Se manda a llamar este método una vez es leído un código QR
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        IntentResult resultado = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
